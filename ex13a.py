25396eeec610987f72d4c17d6475d0026647d9e8
Vasanth <renvasanth@yahoo.co.in>
2015-10-08 15:18:02 +0530

    completed ex13 and ex13a

---
+++ ex13a.py
@@ -0,0 +1,9 @@
+from sys import argv
+script,first, second, third = argv
+first = raw_input()
+second = raw_input()
+third = raw_input()
+print "The script is called:", script
+print "Your first variable is: %r" % first
+print "Your second variable is: %r" % second
+print "Your third variable is: %r" % third
