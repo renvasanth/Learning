ffc59529fa323de52c05504fda014a813749ddcb
Vasanth <renvasanth@yahoo.co.in>
2015-10-08 15:10:01 +0530

    completed bmi.py

---
+++ bmi.py
@@ -0,0 +1,4 @@
+height = float (raw_input("Enter your Height in cm:" ))
+weight = float (raw_input("Enter your Weight in kg:" ))
+BMI = weight/(height**2/10000)
+print "Your Body Mass Index is % d." % BMI
