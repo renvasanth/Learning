7ecdb79cf17b26aa43ec68909c5d8a400e5bca47
renvasanth <renvasanth@yahoo.co.in>
2015-11-19 12:55:01 +0530

    simplified bus project

--- projects/bus.py
+++ projects/bus.py
@@ -3,7 +3,7 @@
 class Route(object):
 
     def __init__(self):
-        self.person = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.person = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         self.a = [1, 2, 3, 4, 5]
         self.b = [6, 7]
         self.c = [8, 9, 10]
@@ -24,21 +24,18 @@ class Route(object):
             print "b = ", self.b
             print "c = ", self.c 
             
-            for per in self.person:
-                for man in self.a:
-                    if man == per:
-                        print "%s is in a" % man
+            for man in self.person:
+                if man in self.a:
+                    print "%s is in a" % man
                 
-                for man in self.b:
-                    if man == per:
-                        print "%s is in b" % man
+                elif man in self.b:
+                    print "%s is in b" % man
                         
-                for man in self.c:
-                    if man == per:
-                        print "%s is in c" % man
+                elif man in self.c:
+                    print "%s is in c" % man
 
 
 if __name__ == "__main__":
     r = Route()
-    r.trip(3)
+    r.trip(5)
 
